5ec0e1418255aac980411b015a1da74d8072f1d9
KhoiNguyen <thorwaitson@gmail.com>
2022-08-26 07:56:21 +0700

    update index.js & add html

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
-const { PORT } = process.env;
+const { PORT = 3000 } = process.env;
 
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/json", (req, res) => {
   res.json({ success: "Hello world!", error: false });
 });
 
